3013dd30a3bdc233cb59c66c0cea68d7accd7de3
Cedric Rochat <rochatc@gmail.com>
2021-04-12 10:11:48 +0000

    Implemented OpenID-Connect connection, after CSCS made this move

--- python_client_example_oidc.py
+++ python_client_example_oidc.py
@@ -1,35 +1,24 @@
 #!/usr/bin/env python
 #
-# This is a sample program on how to use the OpenStack python bindings at CSCS
-#
-#    Copyright (C) 2017, ETH Zuerich, Switzerland
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, version 3 of the License.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#    AUTHOR  Pablo Fernandez 
-#    DATE    September 27th, 2017
-
 # WARNING: this program requires the openstack python libraries installed. Please refer to https://github.com/eth-cscs/openstack/tree/master/cli for instructions.
 
 from keystoneauth1.identity import v3
 from keystoneauth1 import session
 import getpass, os
-from keystoneauth1.extras._saml2 import V3Saml2Password
+from keystoneauth1.identity import V3OidcPassword
+
+#Params
 OS_AUTH_URL = os.environ['OS_AUTH_URL'] if 'OS_AUTH_URL' in os.environ else 'https://pollux.cscs.ch:13000/v3'
 OS_IDENTITY_PROVIDER = os.environ['OS_IDENTITY_PROVIDER'] if 'OS_IDENTITY_PROVIDER' in os.environ else 'cscskc'
-OS_IDENTITY_PROVIDER_URL = os.environ['OS_IDENTITY_PROVIDER_URL'] if 'OS_IDENTITY_PROVIDER_URL' in os.environ else 'https://auth.cscs.ch/auth/realms/cscs/protocol/saml/'
-OS_PROTOCOL = os.environ['OS_PROTOCOL'] if 'OS_PROTOCOL' in os.environ else 'mapped'
+OS_PROTOCOL = os.environ['OS_PROTOCOL'] if 'OS_PROTOCOL' in os.environ else 'openid'
 OS_INTERFACE = os.environ['OS_INTERFACE'] if 'OS_INTERFACE' in os.environ else 'public'
+OS_DISCOVERY_ENDPOINT ='https://auth.cscs.ch/auth/realms/cscs/.well-known/openid-configuration'
+OS_CLIENT_ID = 'pollux-prod'
+OS_CLIENT_SECRET = '82c7a379-f5ee-48c7-8a6b-7ee15557e28e'
+OS_PROTOCOL = 'openid'
+OS_INTERFACE = 'public'
+OS_IDENTITY_API_VERSION=3
+OS_AUTH_URL = 'https://pollux.cscs.ch:13000/v3'
 
 if 'OS_TOKEN' in os.environ:
   # We've already been authenticated. We can just set the right variables
@@ -40,8 +29,15 @@ else:
   ### Authenticate user:
   OS_USERNAME = input("Username: ")
   pw = getpass.getpass()
-  auth = V3Saml2Password(auth_url=OS_AUTH_URL, identity_provider=OS_IDENTITY_PROVIDER, protocol=OS_PROTOCOL, identity_provider_url=OS_IDENTITY_PROVIDER_URL, username=OS_USERNAME, password=pw)
-
+  auth = V3OidcPassword(auth_url=OS_AUTH_URL,
+               identity_provider=OS_IDENTITY_PROVIDER,
+               protocol=OS_PROTOCOL,
+               client_id=OS_CLIENT_ID,
+               client_secret=OS_CLIENT_SECRET,
+               discovery_endpoint=OS_DISCOVERY_ENDPOINT,
+               username=OS_USERNAME,
+               password=pw)
+  
 sess = session.Session(auth=auth)
 print "User info:", OS_USERNAME, sess.get_user_id()
 
@@ -55,8 +51,8 @@ else:
   projects = ks.projects.list(user=sess.get_user_id())
   print "Available projects:", [t.name for t in projects]
   OS_PROJECT_ID = projects[0].id
-
 print "Selected project: ", OS_PROJECT_ID
+
 # (Re-)Scope the session
 auth = v3.Token(auth_url=OS_AUTH_URL, token=sess.get_token(), project_id=OS_PROJECT_ID)
 sess = session.Session(auth=auth)
